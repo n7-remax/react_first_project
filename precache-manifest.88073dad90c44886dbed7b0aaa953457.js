a9721a9976f8ed42f3e586665e9ad38249c7eb12
N7_Remax <ditritusa@gmail.com>
2019-03-17 17:57:48 +0200

    Updates

---
+++ precache-manifest.88073dad90c44886dbed7b0aaa953457.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3deadbc71d053a3e6e58",
+    "url": "/n7-remax/react_first_project/static/js/runtime~main.ae3e6436.js"
+  },
+  {
+    "revision": "cc9aa12d5b1467510dd2",
+    "url": "/n7-remax/react_first_project/static/js/main.65f2c7d0.chunk.js"
+  },
+  {
+    "revision": "7d5f3c14bded133c984d",
+    "url": "/n7-remax/react_first_project/static/js/2.7fdf1c46.chunk.js"
+  },
+  {
+    "revision": "cc9aa12d5b1467510dd2",
+    "url": "/n7-remax/react_first_project/static/css/main.ad86d183.chunk.css"
+  },
+  {
+    "revision": "c4e3ac72ca1eddab2df84c32fa5adeed",
+    "url": "/n7-remax/react_first_project/index.html"
+  }
+];
