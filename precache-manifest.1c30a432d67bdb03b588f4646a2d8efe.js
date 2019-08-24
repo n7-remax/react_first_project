e75a4cf49971f8d9a2f150cd39a1ca34354758ce
N7_Remax <ditritusa@gmail.com>
2019-08-24 18:50:10 +0300

    Updates

---
+++ precache-manifest.1c30a432d67bdb03b588f4646a2d8efe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9218fd503b7afdd7b5821ece4695b14e",
+    "url": "/n7-remax/react_first_project/index.html"
+  },
+  {
+    "revision": "e18956749937c52b9093",
+    "url": "/n7-remax/react_first_project/static/css/main.7bac9dc2.chunk.css"
+  },
+  {
+    "revision": "0e434e53782e4ce434ec",
+    "url": "/n7-remax/react_first_project/static/js/2.8b5a5224.chunk.js"
+  },
+  {
+    "revision": "e18956749937c52b9093",
+    "url": "/n7-remax/react_first_project/static/js/main.8812e1f2.chunk.js"
+  },
+  {
+    "revision": "ebbab82a86430e6c5ce4",
+    "url": "/n7-remax/react_first_project/static/js/runtime~main.2361092f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/n7-remax/react_first_project/precache-manifest.88073dad90c44886dbed7b0aaa953457.js"
+  "/n7-remax/react_first_project/precache-manifest.1c30a432d67bdb03b588f4646a2d8efe.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/n7-remax/react_first_project/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/n7-remax/react_first_project/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
